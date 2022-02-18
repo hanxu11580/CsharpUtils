d429a391a31b667ba184e6391cac322bf8d307dc
DESKTOP-4S2DC32\A <626150455@qq.com>
2022-02-18 15:53:01 +0800

    常用的从原仓库抽出来

---
+++ Utils/HttpUtils.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.IO;
+
+namespace CsharpUtils
+{
+    public static class HttpUtils
+    {
+        public static HttpWebResponse CreateHttpWebResponse(string url, string postData)
+        {
+            var sendBytes = Encoding.UTF8.GetBytes(postData);
+            HttpWebRequest webRequest = (HttpWebRequest)WebRequest.Create(url);
+            webRequest.Method = "POST";
+            webRequest.ContentType = "application/json";
+            webRequest.ContentLength = sendBytes.Length;
+            using (Stream requestStream = webRequest.GetRequestStream())
+            {
+                requestStream.Write(sendBytes, 0, sendBytes.Length);
+            }
+            return (HttpWebResponse)webRequest.GetResponse();
+        }
+
+    }
+}
---
+++ Utils/MathUtils.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace CsharpUtils
+{
+    public static class MathUtils
+    {
+        /// <summary> 是否为偶数 </summary>
+        public static bool IsEvennumber(int val)
+        {
+            return (val & 1) == 0; //与1进行二进制 二进制偶数最后一位为 0
+        }
+    }
+
+}
